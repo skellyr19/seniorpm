190356fa9ba54e8b7b1d80d73364642c6571303f
skellyr19 <44838558+skellyr19@users.noreply.github.com>
2019-03-13 06:55:26 -0400

    More changes

--- src/components/Projects/ProjectDetail.jsx
+++ src/components/Projects/ProjectDetail.jsx
@@ -7,6 +7,9 @@ export class ProjectDetail extends Component {
   constructor(props) {
     super(props);
     this.fetchRecord = this.fetchRecord.bind(this);
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.fetchUser = this.fetchUser.bind(this);
+    this.addTask = this.addTask.bind(this);
     this.state = {
       project: {}
     };
@@ -26,6 +29,8 @@ export class ProjectDetail extends Component {
     if (resp.status >= 200 && resp.status < 300) {
       var project = await resp.json();
       await this.fetchTasks(project);
+      // await this.fetchUser(project);
+      console.log("project from API", project);
       this.setState({ project });
     }
   }
@@ -41,12 +46,49 @@ export class ProjectDetail extends Component {
     ).catch(err => {
       console.log(err);
     });
-    //console.log("Category",resp);
     if (resp.status >= 200 && resp.status < 300) {
       var task = await resp.json();
-      Project.fields.Tasks = task.records;
-      //this.setState({ Category });
+      if (task.records) {
+        Project.fields.Tasks = task.records;
+      }
     }
+  }
+
+  async fetchUser(Project) {
+    var resp = await fetch(
+      airtable.findRecords(
+        "User",
+        "Project",
+        Project.fields.Name,
+        "Grid%20view"
+      )
+    ).catch(err => {
+      console.log(err);
+    });
+    if (resp.status >= 200 && resp.status < 300) {
+      var owner = await resp.json();
+      if (owner.records) {
+        Project.fields.Owner = owner.records[0];
+      }
+      //this.setState({ Project });
+    }
+  }
+
+  async addTask() {
+    // console.log(this.taskName.value);
+    var { project } = this.state;
+    var newTask = {
+      fields: {
+        Name: this.taskName.value,
+        Project: [project.fields.RecId]
+      }
+    };
+    var resp = await fetch(airtable.createRecord("Task", newTask)).catch(
+      err => {
+        console.log(err);
+      }
+    );
+    console.log(resp);
   }
 
   render() {
@@ -56,28 +98,49 @@ export class ProjectDetail extends Component {
         {project && project.fields && project.fields.Name ? (
           <div>
             <h2>
-              {project.fields.Name} ({project.fields.RecId})
+              {project.fields.Name} <small>by {project.fields.OwnerName}</small>
             </h2>
+
             <p>{project.fields.Full}</p>
-            Tasks: {project.fields.Tasks.length}
+            <h5>
+              Tasks
+              <span className="ml-1 badge badge-pill badge-primary">
+                {project.fields.Tasks.length}
+              </span>
+            </h5>
             <ul className="list-group py-2">
-              {project.fields.Tasks && project.fields.Tasks.length > 0
-                ? project.fields.Tasks.map((item, idx) => (
-                    <li
-                      key={idx}
-                      className="list-group-item list-group-item-action d-block py-1"
+              {project.fields.Tasks && project.fields.Tasks.length > 0 ? (
+                project.fields.Tasks.map((item, idx) => (
+                  <li
+                    key={idx}
+                    className="list-group-item list-group-item-action d-block py-1"
+                  >
+                    {item.fields.Name} -{" "}
+                    {!item.fields.Status ? "Not Started" : item.fields.Status}
+                    <Link
+                      className="ml-2 small btn-link text-success"
+                      to={"task/edit/" + item.id}
                     >
-                      {item.fields.Name} -{" "}
-                      {!item.fields.Status ? "Not Started" : item.fields.Status}
-                      <Link
-                        className="ml-2 small btn-link text-success"
-                        to={"task/edit/" + item.id}
-                      >
-                        Edit
-                      </Link>
-                    </li>
-                  ))
-                : ""}
+                      Edit
+                    </Link>
+                  </li>
+                ))
+              ) : (
+                <li className="list-group-item">(No tasks yet...)</li>
+              )}
+              <li className="list-group-item d-inline">
+                <input
+                  className="form-control"
+                  placeholder="Enter task description..."
+                  ref={input => (this.taskName = input)}
+                />
+                <button
+                  className="btn btn-sm btn-primary"
+                  onClick={this.addTask}
+                >
+                  Add task
+                </button>
+              </li>
             </ul>
           </div>
         ) : (
